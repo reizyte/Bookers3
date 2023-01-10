d89e192acc70c30c7806fd002f35363aa0d668ed
reizyte <112692614+reizyte@users.noreply.github.com>
2023-01-10 10:44:28 +0000

    [update]usersコントローラ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   def index
+    @users = user.all
   end
 
   def show
+    @user = user.find(params[:id])
   end
 
   def edit
+    @user = user.find(params[:id])
   end
 end
