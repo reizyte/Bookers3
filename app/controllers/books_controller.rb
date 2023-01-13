a7800fa4fc904ef5da60344c7ec119a119ae6b8a
reizyte <112692614+reizyte@users.noreply.github.com>
2023-01-13 07:58:52 +0000

    [add]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,32 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to user_path(current_user)
+    end
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
@@ -20,6 +21,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
+    params.require(:user).permit(:name, :caption, :profile_image)
   end
 end
